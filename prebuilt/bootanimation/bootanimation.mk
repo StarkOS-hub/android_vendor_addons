0007182b35a0a019d2b5b74457090451ab40cf03
StarkOS <starkos.git@gmail.com>
2024-09-27 23:45:05 +0530

    Create bootanimation.mk

---
+++ prebuilt/bootanimation/bootanimation.mk
@@ -0,0 +1,13 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),720)
+    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 720p as default")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 720p as default")
+    endif
+    # Default to 720p if no resolution is defined or supported
+    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
