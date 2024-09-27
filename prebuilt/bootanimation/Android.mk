ad5473ca1fd6824ed0791acb3e362de2118c69cd
StarkOS <starkos.git@gmail.com>
2024-09-27 22:01:27 +0530

    for now only 720p res

--- prebuilt/bootanimation/Android.mk
+++ prebuilt/bootanimation/Android.mk
@@ -1,14 +1,6 @@
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
     PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),1080)
-    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
-else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),1440)
-    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else ifeq ($(TARGET_PIXEL_BOOT_ANIMATION_RES),720)
     PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
 else
