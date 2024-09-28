bd51e7710b0e60ccac6f54ab485ea8e72186ebd6
StarkOS <starkos.git@gmail.com>
2024-09-28 09:34:28 +0530

    fixed build errors with bootanimation

--- prebuilt/bootanimation/bootanimation.mk
+++ prebuilt/bootanimation/bootanimation.mk
@@ -1,12 +1,12 @@
 # Bootanimation for 720p
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
         $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 720p")
-        PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+        PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_OUT_PRODUCT)/media/bootanimation.zip
     else
         $(warning "Current bootanimation res is not supported, forcing 720p")
-        PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+        PRODUCT_COPY_FILES += vendor/addons/prebuilt/bootanimation/bootanimation_720.zip:$(TARGET_OUT_PRODUCT)/media/bootanimation.zip
     endif
 endif
